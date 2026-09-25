b0609414dce8097c4b86ae65a4cd870cdeb0cc1f
javaprogrammers35 <0987bvg@gmail.com>
2026-09-25 20:14:23 +0300

    refactor: remove App and Router components to streamline application structure

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider } from '@tanstack/react-router';
-import { router } from './router/Router';
-
-export type { TimerSnapshot } from './router/Router';
-
-const App = () => <RouterProvider router={router} />;
-
-export default App;
