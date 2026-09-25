3faee89164bd6dd4a728787fbe4b43a1fb28e008
javaprogrammers35 <0987bvg@gmail.com>
2026-09-25 20:22:39 +0300

    feat: integrate tanstackRouter with AppStateContext in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
-import { I18nProvider } from './i18n/I18nProvider';
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+import { AppStateProvider } from './context/AppStateContext'
+import { I18nProvider } from './i18n/I18nProvider'
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <I18nProvider>
-      <App />
+      <AppStateProvider>
+        <RouterProvider router={router} />
+      </AppStateProvider>
     </I18nProvider>
   </StrictMode>,
 )
